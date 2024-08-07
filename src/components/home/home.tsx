26087266d78983c4955252d7c84785b1bac9b6f8
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-08-07 15:24:45 +0200

    ok

---
+++ src/components/home/home.tsx
@@ -0,0 +1,18 @@
+import './home.scss'
+
+function Home() {
+
+  return (
+    <>
+    <section id="home">
+      <h1>HELLO WORLD</h1>
+
+      <h3>What a beautifull page!!!</h3>
+
+      <p>Wow...</p>
+    </section>
+    </>
+  )
+}
+
+export default Home
